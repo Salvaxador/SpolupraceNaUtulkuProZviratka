ad4226d4c67088dcde6d1d72ba8759d192c94463
bbbbali <scarlexmail@gmail.com>
2025-11-27 19:52:57 +0100

    pridani switche

--- Program.cs
+++ Program.cs
@@ -27,7 +27,19 @@ namespace SpolupraceNaUtulkuProZviratka
 
                 string volba = Console.ReadLine();
 
-                
+                switch (volba)
+                {
+                    case "1": VypisClass.PridatZvire(service); break;
+                    case "2": VypisClass.VypisVsechna(service); break;
+                    case "3": VypisClass.Vyhledat(service); break;
+                    case "4": VypisClass.Adopce(service); break;
+                    case "5": VypisClass.Statistiky(service); break;
+                    case "0": return;
+                    default: Console.WriteLine("Neplatná volba."); break;
+                }
+
+                Console.WriteLine("Pokračuj stiskem klávesy...");
+                Console.ReadKey();
             }
         }
     }
